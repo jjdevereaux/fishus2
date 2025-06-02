69f97e6729d13c67cd54d7a7081c26be66fbc1ef
LAPTOP-H8NH8T89\matte <matteumayo@gmail.com>
2025-06-01 19:57:34 -0700

    UI scene initial progress, UI sprites

---
+++ client-unity/Assets/Scripts/UI/InventoryWindowManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+
+public class InventoryWindowManager : MonoBehaviour
+{
+    // This class manages the construction of the inventory UI.
+    [SerializeField]
+    private int inventoryWindowSizeX, inventoryWindowSizeY, itemSlotSize;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
